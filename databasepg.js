0aea972485e6561d9e763a553543eed4a437b838
kshuhambgs123 <kshubhambgs123@gmail.com>
2023-09-22 22:34:31 +0530

    Added Database Connection

--- databasepg.js
+++ databasepg.js
@@ -2,15 +2,15 @@ const {Client} = require('pg');
 
 const client = new Client({
     host: "localhost",
-    user: "postgres",
+    user: "shubhamkumar",
     port: 5432,
-    password: "rootUser",
-    database: "postgres"
+    password: "shubham123",
+    database: "shubhamkumar"
 })
 
 client.connect();
 
-client.query(`Select * from users`, (err,res)=>{
+client.query(`Select * from book`, (err,res)=>{
 if(!err){
     console.log(res.rows);
 }else{
