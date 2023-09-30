40bf62513ecbd69611726e4d5614b32c70bbe60d
kshuhambgs123 <kshubhambgs123@gmail.com>
2023-10-01 00:20:24 +0530

    added config for migration

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-// module.exports={
-//     "development":{
-//         dialect: 'postgres', // Use the PostgreSQL dialect
-//         host: 'localhost', // Your database host
-//         username: 'shubhamkumar', // Your database username
-//         password: 'shubham123', // Your database password
-//         database: 'shubhamkumar', // Your database name
-//         port:5432
-//       }
-//   }
+module.exports={
+    "development":{
+        dialect: 'postgres', // Use the PostgreSQL dialect
+        host: 'localhost', // Your database host
+        username: 'shubhamkumar', // Your database username
+        password: 'shubham123', // Your database password
+        database: 'shubhamkumar', // Your database name
+        port:5432
+      }
+  }
   
